4a9e7d3b141e1aec932793faf62ba338c8f4a7b5
septe habudin <septe.vhirgo@gmail.com>
2022-12-25 21:01:59 +0700

    remove file default

--- Tests/CorePackageTests/CorePackageTests.swift
+++ Tests/CorePackageTests/CorePackageTests.swift
@@ -6,6 +6,6 @@ final class CorePackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CorePackage().text, "Hello, World!")
+//        XCTAssertEqual(CorePackage().text, "Hello, World!")
     }
 }
